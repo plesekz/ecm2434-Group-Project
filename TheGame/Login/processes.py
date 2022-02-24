9204a9be1b0fa8f8b02f7dd07c4e325911bd5e0e
Tz01234 <92532040+Tz01234@users.noreply.github.com>
2022-02-24 18:19:32 +0000

    Added more error messages

--- TheGame/Login/processes.py
+++ TheGame/Login/processes.py
@@ -1,25 +1,36 @@
+from email.message import EmailMessage
 from django.shortcuts import redirect
 from .forms import PlayerForm
 from .models import Player
 from django.contrib import messages
+from django.contrib.auth import authenticate
 
 def validateLogIn(request):
     if not request.method == "POST":
+        messages.success(request, ('Something went wrong, please try again later'))
         return "failed to process, please use POST method"
 
-    email = request.POST['email']
-    password = request.POST['password']
+    _email = request.POST['email']
+    _username = request.POST['username']
+    _password = request.POST['password']
+
+    if Player.objects.filter(email=_email).exists():
+        user = authenticate(request, username=_username, password=_password)
+        if user is not None:
+            response = redirect("login")
+            response.set_cookie('TheGameSessionID', 'cookie_value')
+        else:
+            messages.success(request, ('Incorrect password, try again'))
+    else:
+        messages.success(request, ('Username entered doesn\'t exist'))
 
     #proccess log in
-
-    response = redirect("login")
-    response.set_cookie('cookie_name', 'cookie_value')
-
 
     return response
 
 def validateRegister(request):
     if not request.method == "POST":
+        messages.success(request, ('Something went wrong, please try again later'))
         return "failed to process, please use POST method"
     form = PlayerForm(request.POST or None)
 
@@ -28,9 +39,11 @@ def validateRegister(request):
     username = request.POST['username']
 
     if Player.objects.filter(email=email).exists():
-        return redirect("register")#return "Email is taken"
+        messages.success(request, ('The Email you chose is taken'))
+        return redirect("register")
     if Player.objects.filter(username=username).exists():
-        return redirect("register")#return "Username is taken"
+        messages.success(request, ('The Username you chose is taken'))
+        return redirect("register")
 
     if form.is_valid():
         form.save()
