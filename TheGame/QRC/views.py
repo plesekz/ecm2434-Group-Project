54e66495a508dffe5e0e3a37f72e8b173bc09c49
Zdeněk Plešek <7denek.p1esek@gmail.com>
2022-02-28 01:25:54 +0000

    Further progress on the page
    TODO:
        Implement JS that handles the requests
        Change the placeholder in views once model is implemented

--- TheGame/QRC/views.py
+++ TheGame/QRC/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def QR_management(request):
+    codes = get_object_or_404(PLACEHOLDER_FOR_CODES)
+    return render(request, 'QRC/manage.html', codes)
