41ee7f32009b8c6eb0268b537b70de345e9d5161
Zdeněk Plešek <7denek.p1esek@gmail.com>
2022-02-28 21:00:16 +0000

    Update views.py
    views updated for the management page

--- TheGame/QRC/views.py
+++ TheGame/QRC/views.py
@@ -4,9 +4,14 @@ from django.template import loader
 
 # Create your views here.
 
-#def QR_management(request):
-#    codes = get_object_or_404(PLACEHOLDER_FOR_CODES)
-#    return render(request, 'QRC/manage.html', codes)
+def QR_management(request):
+    codes = get_object_or_404(PLACEHOLDER_FOR_CODES)
+    list_of_resources = get_object_or_404(PLACEHOLDER_FOR_ALL_RESOURCE_TYPES_IN_THE_RESOURCE_MANAGEMENT_MODULE)
+    dict = {
+        "codes" : codes,
+        "list_of_resources" : list_of_resources
+    }
+    return render(request, 'QRC/manage.html', dict)
 
 def qr_landing(request):
     template = loader.get_template("QRC/landing.html")
