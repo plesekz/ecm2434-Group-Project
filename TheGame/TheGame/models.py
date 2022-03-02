e6324292bb7fcca8030f0dfa0c65b6625ddbc15a
Tz01234 <92532040+Tz01234@users.noreply.github.com>
2022-03-02 15:04:05 +0000

    Added more logical default stats for the player

--- TheGame/TheGame/models.py
+++ TheGame/TheGame/models.py
@@ -3,12 +3,12 @@ from Login.models import Player
 
 class pStat(models.Model):
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
-    pHealth = models.PositiveIntegerField(default=0)
-    pToughness = models.PositiveIntegerField(default=0)
+    pHealth = models.PositiveIntegerField(default=100)
+    pToughness = models.PositiveIntegerField(default=1)
     pEvasion = models.PositiveIntegerField(default=0)
-    damage = models.PositiveIntegerField(default=0)
-    accuracy = models.PositiveIntegerField(default=0)
-    attackSpeed = models.PositiveIntegerField(default=0)
+    damage = models.PositiveIntegerField(default=1)
+    accuracy = models.PositiveIntegerField(default=1)
+    attackSpeed = models.PositiveIntegerField(default=1)
     aHealth = models.PositiveIntegerField(default=0)
     aToughness = models.PositiveIntegerField(default=0)
     aEvasion = models.PositiveIntegerField(default=0)
