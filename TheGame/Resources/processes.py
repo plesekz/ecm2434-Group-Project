228d9baa354d4f82b9034c4060c9f557e6f234ec
Alex <billybo1234@outlook.com>
2022-03-02 09:19:36 +0000

    added getResourceByName

--- TheGame/Resources/processes.py
+++ TheGame/Resources/processes.py
@@ -1,3 +1,4 @@
+from tokenize import String
 from django.http import HttpResponse, HttpResponseRedirect
 from Resources.models import PlayerResource, Resource
 from Login.models import Player
@@ -68,3 +69,9 @@ def getAllUserResources(user : Player) -> "list[tuple[Resource, int]]":
         resourceList.append((pr.resource, pr.amount))
 
     return resourceList
+
+def getResourceByName(name : String) -> Resource:
+    if (not (res := Resource.objects.filter(name=name)).exists()):
+        return None
+
+    return res
