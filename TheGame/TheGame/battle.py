275cbfff9e0366ad9a984a7fde4e7480b0521d83
UndeadP0WER <UndeadP0WER@outlook.com>
2022-03-02 21:46:22 +0000

    battle.py broken?
    why does average joe always win when stats are the same

--- TheGame/TheGame/battle.py
+++ TheGame/TheGame/battle.py
@@ -1,6 +1,7 @@
 from random import randint
 from TheGame.processes import getUserFromName
 from django.shortcuts import redirect
+from django.contrib import messages
 import TheGame.models
 import math
 
@@ -10,8 +11,8 @@ def take_turn(player, target):
         roll = randint(1,100)
         if ((player.acc - target.ev) >= roll):
             #hit
-            target.hp -= math.ceil((player.num/(target.tg / 100)))
-        #print(f"Attack {i} rolled {roll} against {(player.acc - target.ev)}, leaving opponent on {target.hp}")
+            target.hp -= math.ceil((player.dmg/(target.tg / 100)))
+        return (f"Attack {i} rolled {roll} against {(player.acc - target.ev)}, leaving opponent on {target.hp}")
 
 def check_if_dead(player):
     if (player.hp <= 0):
@@ -26,14 +27,21 @@ def getLocalPlayer(request):
 def getOpponent(request):
     #ops = getUserFromName(request) #This would need to be made to access the PVE database
     #oppPlayer = Player(ops)
-    oppPlayer = PseudoPlayer(100, 100, 10, 2, 100, 1, 0, 0, 0)
+    oppPlayer = PseudoPlayer(100, 1, 10, 2, 100, 1, 0, 99, 0)
     return oppPlayer
 
 def callBattle(request):
-    battle(getLocalPlayer(request), getOpponent(request))
-    return redirect("/")
+    log = battle(getLocalPlayer(request), getOpponent(request))
+    p = getLocalPlayer(request)
+    if log[0]:
+        messages.success(request, (f'Victory with stats hp {p.hp}, tg {p.tg}, ev {p.ev}, dmg {p.dmg}, acc {p.acc}, num  {p.num}, log <br> {log[1]}'))
+        return redirect("/battleSelect")
+    messages.success(request, (f'Loss with stats hp {p.hp}, tg {p.tg}, ev {p.ev}, dmg {p.dmg}, acc {p.acc}, num  {p.num}, log <br> {log[1]}'))
+    return redirect("/battleSelect")
 
 def battle(p1, p2):
+    log = [False, ""]
+    
     #redundant as it takes a Player input now
     ##to collapse the defensive stats once rather than every time its called
     #p1 = Player(player1)
@@ -43,29 +51,41 @@ def battle(p1, p2):
     
     #decides who goes first randomly
     #50% chance p1 goes first, 50% chance p2 goes first
-    if (p1.num > p2.num): #I made it so the player with faster attack speed goes first #randint(0,1) == 1):
+    if (p1.num > p2.num): #I made it so the player with faster attack speed goes first
         #player 1's turn
         if check_if_dead(p1):
-            return False #if player 2 wins
-        take_turn(p1,p2)
+            log[0] = False
+            return log
+            #return False #if player 2 wins
+        log[1] += " <br> p1:"
+        log[1] += take_turn(p1,p2)
     elif (p1.num == p2.num): #if speed tie,
         if (randint(0,1) == 1): #50% chance of each player going first
             if check_if_dead(p1):
-                return False #if player 2 wins
-        take_turn(p2,p1)
+                log[0] = False
+                return log
+                #return False #if player 2 wins
+        log[1] += " <br> p1:"
+        log[1] += take_turn(p1,p2)
             
     
     #loop forever, breaks out when a player is dead
     while(True):
         #player 2's turn
         if check_if_dead(p2):
-            return True #if player 1 wins
-        take_turn(p2,p1)
+            log[0] = False
+            return log
+            #return True #if player 1 wins
+        log[1] += " <br> p2:"
+        log[1] += take_turn(p2,p1)
         
         #player 1's turn
         if check_if_dead(p1):
-            return False #if player 2 wins
-        take_turn(p1,p2)
+            log[0] = False
+            return log
+            #return False #if player 2 wins
+        log[1] += " <br> p1:"
+        log[1] += take_turn(p1,p2)
 
 class Player:
     def __init__(self, playerStats):
