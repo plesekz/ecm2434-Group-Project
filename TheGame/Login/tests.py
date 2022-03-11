bb475343d8785b117e43da4716088d1ba2aebe67
ABaker1343 <billybo1234@outlook.com>
2022-03-11 14:44:06 +0000

    started login tests

--- TheGame/Login/tests.py
+++ TheGame/Login/tests.py
@@ -1,12 +1,37 @@
-from django.test import TestCase
+from time import sleep
+from django.http import HttpRequest
+from django.test import TestCase, RequestFactory, Client
+from django.contrib.auth.models import AnonymousUser
 from Login.models import Player
+from Login.processes import validateRegister
+import requests
+import json
 
 class LoginTestCase(TestCase):
 
     def setUp(self):
-        self.p1 = Player.objects.create(
-            
+        return None
+
+    def test_registration(self):
+
+        client = Client()
+        # send a request to the validate login page
+        client.post(
+            "/login/validateRegister/",
+            data={
+                'email' : "test@email.com",
+                'username' : "testUsername",
+                'password': "testPassword",
+                'confirmPassword' : "testPassword"
+            },
+            content_type='application/json'
         )
 
-    def test_():
-        assert False
+        #check that the user was added to the database
+        sleep(2)
+
+        try:
+            user = Player.objects.get(username="testUsername")
+            assert True
+        except:
+            assert False
