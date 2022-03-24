1ced3fdc64dd94cb87233f676b0ee346bd55f86f
Tz01234 <92532040+Tz01234@users.noreply.github.com>
2022-03-24 09:41:44 +0000

    Revert "Disable debug"
    This reverts commit c22f896677ae709a9e8db5dcb5a646c3ed682971.

--- TheGame/TheGame/settings.py
+++ TheGame/TheGame/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3!5w%r8!pke8szc2e&(tz-x0#hok^5c=o+6@zzb^$#(m*k-yne'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 # Fix access from different address
